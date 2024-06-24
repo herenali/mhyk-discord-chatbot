06a2a414a44a6a76f380c4b49d45435f4d107d6a
herenali <herenalihoiching@gmail.com>
2024-06-24 11:32:39 +0800

    Edit clean_data.py and add short version of script

--- clean_data.py
+++ clean_data.py
@@ -1,22 +1,22 @@
 import csv
 import re
 
-with open("mhyk-main-story-script.txt") as text_file:
+with open("mhyk-main-story-script-short.txt") as text_file:  # TODO: Change txt file
     with open("mhyk-name-line.csv", "w") as out_file:
         writer = csv.writer(out_file)
         writer.writerow(('name', 'line'))
 
         line = text_file.readline()
         while line != "":
-            if line == '':
-                break
+            # if line == '':
+            #     break
 
             line_lst = []
             if line != "\n":
                 line = line.strip("\n")
 
                 if ":" in line:
-                    line_lst = re.split(": ", line)
+                    line_lst = re.split(":\xa0", line)
                     line = text_file.readline()
                 elif re.search("[0-9]", line) is None and re.search("[a-zA-Z]", line) is not None:
                     line_lst.append('Narrator')
@@ -31,7 +31,7 @@ with open("mhyk-main-story-script.txt") as text_file:
                     line_lst[-1] += " " + line
                     line = text_file.readline()
                 print(line_lst)
-                # writer.writerow(line_lst)
+                writer.writerow(line_lst)
 
             line = text_file.readline()
 
