05f05289bbfe155ba2e795628b66c97a55a49595
herenali <herenalihoiching@gmail.com>
2024-06-24 11:40:20 +0800

    Fix bugs in clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -4,32 +4,30 @@ import re
 with open("mhyk-main-story-script-short.txt") as text_file:  # TODO: Change txt file
     with open("mhyk-name-line.csv", "w") as out_file:
         writer = csv.writer(out_file)
-        writer.writerow(('name', 'line'))
+        writer.writerow(("name", "line"))
 
         line = text_file.readline()
         while line != "":
-            # if line == '':
-            #     break
-
             line_lst = []
             if line != "\n":
                 line = line.strip("\n")
 
-                if ":" in line:
+                if ":" in line and re.search("[0-9]", line) is None:
                     line_lst = re.split(":\xa0", line)
                     line = text_file.readline()
                 elif re.search("[0-9]", line) is None and re.search("[a-zA-Z]", line) is not None:
-                    line_lst.append('Narrator')
+                    line_lst.append("Narrator")
                     line_lst.append(line)
                     line = text_file.readline()
                 else:
                     line = text_file.readline()
                     continue
 
-                while line != "\n":
+                while line != "\n" and line != "":
                     line = line.strip("\n")
                     line_lst[-1] += " " + line
                     line = text_file.readline()
+
                 print(line_lst)
                 writer.writerow(line_lst)
 
